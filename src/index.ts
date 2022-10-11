26578bfa06314f2381637fda426270746fe27945
“Altarya DeFlammes„ <blazethecatdflammes@outlook.com>
2022-10-11 18:31:02 -0300

    basic layers

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,75 @@
 import * as L from "leaflet";
 import { LatLngBounds, LatLngBoundsExpression, LatLngExpression, LeafletMouseEvent, ImageOverlayOptions } from "leaflet";
 
-let map = new L.Map('map', {
-    crs: L.CRS.Simple,
-    center: new L.LatLng(3374, 3339),
-    zoom: 1,
-    minZoom: -3
-});
-
-var southWest: LatLngExpression = new L.LatLng(0, 0)
-var northEast: LatLngExpression = new L.LatLng(5120, 9728)
-
-var terrain = L.imageOverlay(
-    './assets/layers/terrain.webp', 
-    L.latLngBounds( southWest, northEast),
-    {
-        zIndex: 0
+fetch("./assets/layers/tropics.svg").then((response => {
+    if (response.ok) {
+        return response.blob()
+        //tropicsFile = URL.createObjectURL(await response.blob()).toString()
     }
-).addTo(map)
+    }))
+    .then((result) => {
+        result.text().then(response => {
 
-//map.fitBounds(L.latLngBounds( southWest, northEast));
-//map.fitWorld()
+            let map = new L.Map('map', {
+                crs: L.CRS.Simple,
+                center: new L.LatLng(3374, 3339),
+                zoom: 0,
+                minZoom: -3
+            });
+            
+            var layerController = L.control.layers().addTo(map)
+            
+            var southWest: LatLngExpression = new L.LatLng(0, 0)
+            var northEast: LatLngExpression = new L.LatLng(5120, 9728)
+            
+            var terrain = L.imageOverlay(
+                './assets/layers/terrain.webp', 
+                L.latLngBounds( southWest, northEast),
+                {
+                    zIndex: 0
+                }
+            ).addTo(map)
 
-map.on('click', (e: LeafletMouseEvent) => {
-    let marker = L.marker(e.latlng)
-    
-    .bindPopup("You clicked the map at " + e.latlng.toString())
-    .addTo(map)
-    .openPopup();
-});
+            var municipalities = L.imageOverlay(
+                './assets/layers/municipalities.webp', 
+                L.latLngBounds( southWest, northEast),
+                {
+                    zIndex: 1
+                }
+            )
+            var states = L.imageOverlay(
+                './assets/layers/states.webp', 
+                L.latLngBounds( southWest, northEast),
+                {
+                    zIndex: 2
+                }
+            )
+
+            var tropicsParse = new DOMParser()
+            var tropicsDoc = tropicsParse.parseFromString(response, "image/svg+xml")
+            var tropicsElem:SVGAElement = <SVGAElement>tropicsDoc.getElementsByTagNameNS("http://www.w3.org/2000/svg", "svg").item(0);
+
+            var tropics = L.svgOverlay(
+                tropicsElem,
+                L.latLngBounds( southWest, northEast),
+                {
+                    zIndex: 3
+                }
+            )
+
+            layerController.addOverlay(municipalities, "Municipalities, Sub-states and Sea Regions")
+            layerController.addOverlay(states, "Administrative Divisions(States)")
+            layerController.addOverlay(tropics, "Tropics")
+
+            map.on('click', (e: LeafletMouseEvent) => {
+                let marker = L.marker(e.latlng)
+                
+                .bindPopup("You clicked the map at " + e.latlng.toString())
+                .addTo(map)
+                .openPopup();
+            });
+        })
+    }
+)
+
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
       },
     ],
   },
+  mode: 'development',
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
