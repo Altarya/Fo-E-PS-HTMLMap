00815d9c9ddb4b8d902c193c6ca8733ed8a722e2
“Altarya DeFlammes„ <blazethecatdflammes@outlook.com>
2022-10-13 13:38:55 -0300

    Continents and oceans layer

--- src/index.ts
+++ src/index.ts
@@ -8,77 +8,100 @@ require('./search.ts')
 fetch("./assets/layers/tropics.svg").then((response => {
     if (response.ok) {
         return response.blob()
-        //tropicsFile = URL.createObjectURL(await response.blob()).toString()
     }
     }))
     .then((result) => {
         result.text().then(response => {
 
-            let map = new L.Map('map', {
-                crs: L.CRS.Simple,
-                center: new L.LatLng(3374, 3339),
-                zoom: 0,
-                minZoom: -3
-            });
-            
-            var layerController = L.control.layers().addTo(map)
-            
-            var southWest: LatLngExpression = new L.LatLng(0, 0)
-            var northEast: LatLngExpression = new L.LatLng(5120, 9728)
-            
-            var terrain = L.imageOverlay(
-                './assets/layers/terrain.webp', 
-                L.latLngBounds( southWest, northEast),
-                {
-                    zIndex: 0
+            fetch("./assets/layers/continentsoceans.svg").then((response => {
+                if (response.ok) {
+                    return response.blob()
                 }
-            ).addTo(map)
+                }))
+                .then((result) => {
+                    result.text().then(response => {
 
-            var municipalities = L.imageOverlay(
-                './assets/layers/municipalities.webp', 
-                L.latLngBounds( southWest, northEast),
-                {
-                    zIndex: 1
+                        let map = new L.Map('map', {
+                            crs: L.CRS.Simple,
+                            center: new L.LatLng(3374, 3339),
+                            zoom: 0,
+                            minZoom: -3
+                        });
+                        
+                        var layerController = L.control.layers().addTo(map)
+                        
+                        var southWest: LatLngExpression = new L.LatLng(0, 0)
+                        var northEast: LatLngExpression = new L.LatLng(5120, 9728)
+                        
+                        var terrain = L.imageOverlay(
+                            './assets/layers/terrain.webp', 
+                            L.latLngBounds( southWest, northEast),
+                            {
+                                zIndex: 0
+                            }
+                        ).addTo(map)
+
+                        var municipalities = L.imageOverlay(
+                            './assets/layers/municipalities.webp', 
+                            L.latLngBounds( southWest, northEast),
+                            {
+                                zIndex: 1
+                            }
+                        )
+                        var states = L.imageOverlay(
+                            './assets/layers/states.webp', 
+                            L.latLngBounds( southWest, northEast),
+                            {
+                                zIndex: 2
+                            }
+                        )
+
+                        var tropicsParse = new DOMParser()
+                        var tropicsDoc = tropicsParse.parseFromString(response, "image/svg+xml")
+                        var tropicsElem:SVGAElement = <SVGAElement>tropicsDoc.getElementsByTagNameNS("http://www.w3.org/2000/svg", "svg").item(0);
+
+                        var tropics = L.svgOverlay(
+                            tropicsElem,
+                            L.latLngBounds( southWest, northEast),
+                            {
+                                zIndex: 3
+                            }
+                        )
+
+                        var continentsParse = new DOMParser()
+                        var continentsDoc = continentsParse.parseFromString(response, "image/svg+xml")
+                        var continentsElem:SVGAElement = <SVGAElement>continentsDoc.getElementsByTagNameNS("http://www.w3.org/2000/svg", "svg").item(0);
+
+                        var continents = L.svgOverlay(
+                            continentsElem,
+                            L.latLngBounds( southWest, northEast),
+                            {
+                                zIndex: 3
+                            }
+                        )
+
+                        layerController.addOverlay(municipalities, "Municipalities, Sub-states and Sea Regions")
+                        layerController.addOverlay(states, "Administrative Divisions(States)")
+                        layerController.addOverlay(tropics, "Tropics")
+                        layerController.addOverlay(continents, "Continents and Oceans")
+
+                        map.on('click', (e: LeafletMouseEvent) => {
+                            let marker = L.marker(e.latlng)
+                            
+                            .bindPopup("You clicked the map at " + e.latlng.toString())
+                            .addTo(map)
+                            .openPopup();
+                        });
+
+                        var poiLayer = setupPOI(map, layerController);
+
+                        map.addControl( new L.Control.Search({layer: poiLayer, zoom: 5}) );
+                        //searchLayer is a L.LayerGroup contains searched markers
+
+                        console.log("Loaded Search")
+                    })
                 }
             )
-            var states = L.imageOverlay(
-                './assets/layers/states.webp', 
-                L.latLngBounds( southWest, northEast),
-                {
-                    zIndex: 2
-                }
-            )
-
-            var tropicsParse = new DOMParser()
-            var tropicsDoc = tropicsParse.parseFromString(response, "image/svg+xml")
-            var tropicsElem:SVGAElement = <SVGAElement>tropicsDoc.getElementsByTagNameNS("http://www.w3.org/2000/svg", "svg").item(0);
-
-            var tropics = L.svgOverlay(
-                tropicsElem,
-                L.latLngBounds( southWest, northEast),
-                {
-                    zIndex: 3
-                }
-            )
-
-            layerController.addOverlay(municipalities, "Municipalities, Sub-states and Sea Regions")
-            layerController.addOverlay(states, "Administrative Divisions(States)")
-            layerController.addOverlay(tropics, "Tropics")
-
-            map.on('click', (e: LeafletMouseEvent) => {
-                let marker = L.marker(e.latlng)
-                
-                .bindPopup("You clicked the map at " + e.latlng.toString())
-                .addTo(map)
-                .openPopup();
-            });
-
-            var poiLayer = setupPOI(map, layerController);
-
-            map.addControl( new L.Control.Search({layer: poiLayer, zoom: 5}) );
-            //searchLayer is a L.LayerGroup contains searched markers
-
-            console.log("Loaded Search")
         })
     }
 )
